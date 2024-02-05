8d6c23b9e5eeea402365116c00b84ff9a773fb73
Ajay7616 <u.ajaykumar7616@gmail.com>
2024-02-05 17:58:21 +0530

    Logout function added

--- client/src/components/NavIcons/NavIcons.jsx
+++ client/src/components/NavIcons/NavIcons.jsx
@@ -3,10 +3,17 @@ import React from "react";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
+import Logout from "../../img/logout.png";
 import { UilSetting } from "@iconscout/react-unicons";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../actions/AuthAction";
 import { Link } from "react-router-dom";
 
 const NavIcons = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
   return (
     <div className="navIcons">
       <Link to="../home">
@@ -16,6 +23,9 @@ const NavIcons = () => {
       <img src={Noti} alt="" />
       <Link to="../chat">
         <img src={Comment} alt="" />
+      </Link>
+      <Link onClick={handleLogout}>
+        <img src={Logout} alt="" />
       </Link>
     </div>
   );
--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -3,12 +3,19 @@ import "./RightSide.css";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
+import Logout from "../../img/logout.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
 import ShareModal from "../ShareModal/ShareModal";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../actions/AuthAction";
 import { Link } from "react-router-dom";
 
 const RightSide = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
   const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="RightSide">
@@ -21,7 +28,9 @@ const RightSide = () => {
         <Link to="../chat">
           <img src={Comment} alt="" />
         </Link>
-        
+        <Link onClick={handleLogout}>
+          <img src={Logout} alt="" />
+        </Link> 
       </div>
 
       <TrendCard />
